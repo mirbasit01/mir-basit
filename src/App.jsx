459bfe5423f1da8d3853d997f90e0befdf3ed5d2
Abdul Basit <121716860+mirbasit01@users.noreply.github.com>
2025-01-10 18:43:49 +0500

    Hear from My Clients

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from './section/Navbar'
 import Hero from './section/Hero'
-
+import About from './section/About'
+import Projects from './section/Projects'
+import Clients from './section/Clients'
+ 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
       <Navbar/>
       <Hero/>
+      <About/>
+      <Projects/>
+      <Clients/>
     </main>
     
   )
--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -109,7 +109,7 @@ const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
     </div>
 
       <div className='absolute bottom-7 left-0 right-0 w-full z-101 c-space'>
-          <a href='#contact' className='w-fit'>
+          <a href='#about' className='w-fit'>
                <Button  name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96"/>
           </a>
 
